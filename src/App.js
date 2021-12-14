a14f934db9eda7ee9a311609f1570f243f1bf6d2
servivo <91890647+servivo@users.noreply.github.com>
2021-12-14 13:12:41 +0100

    About the project added

--- src/App.js
+++ src/App.js
@@ -12,7 +12,8 @@ export default function App() {
         </main>
         <section className="about">
           <h4>About the project</h4>
-          bleblablabalbla
+          My name is Beatriz and I started learning German when I was 7 years old. Back then, our teachers always encouraged us to use German to German dictionaries as a way to enhance our vocabularies and shift into thinking in German instead of translating everything in our heads. Now I live in Germany and have many expat friends learning German at adult age. I decided to create this dictionary for them as the final project of my React course. This app is suitable for people of all ages learning German. It works with the <a href="https://dictionaryapi.dev/" target="_blank"
+              rel="noopener noreferrer"> Free Dictionary APi </a> which currently supports phonetic transcription, word class, word definition and usage examples for German language. The page won't reload when a word that is not saved in the API is typed. I strive to keep on improving this page and helping others to learn German. Viel Erfolg! 🍀 
         </section>
         <footer className="App-footer">
           <small>
