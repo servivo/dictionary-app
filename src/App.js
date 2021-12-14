4a0ca70d19d065fd4b3ae6551a84b0531a694490
servivo <91890647+servivo@users.noreply.github.com>
2021-12-14 23:40:10 +0100

    Small changes project description

--- src/App.js
+++ src/App.js
@@ -12,8 +12,35 @@ export default function App() {
         </main>
         <section className="about">
           <h4>About the project</h4>
-          My name is Beatriz and I started learning German when I was 7 years old. Back then, our teachers always encouraged us to use German to German dictionaries as a way to enhance our vocabularies and shift into thinking in German instead of translating everything in our heads. Now I live in Germany and have many expat friends learning German at adult age. I decided to create this dictionary for them as the final project of my React course. This app is suitable for people of all ages learning German. It works with the <a href="https://dictionaryapi.dev/" target="_blank"
-              rel="noopener noreferrer"> Free Dictionary APi </a> which currently supports phonetic transcription, word class, word definition and usage examples for German language. The page won't reload when a word that is not saved in the API is typed. I strive to keep on improving this page and helping others to learn German. Viel Erfolg! 🍀 
+          My name is Beatriz and I started learning German when I was 7 years
+          old. Back then, our teachers always encouraged us to use German to
+          German dictionaries as a way to enhance our vocabularies and shift
+          into thinking in German instead of translating everything in our
+          heads. Now I live in Germany and have many expat friends learning
+          German at adult age. I decided to create this dictionary for them as
+          the final project of my React course. This app is suitable for people
+          of all ages learning German. It works with the{" "}
+          <a
+            href="https://dictionaryapi.dev/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            Free Dictionary APi{" "}
+          </a>{" "}
+          which currently supports phonetic transcription, word class, word
+          definition and usage examples for German language. The page won't
+          reload when a word that is not saved in the API is typed. For the
+          photos I am working with the{" "}
+          <a
+            href="https://www.pexels.com/de-de/api/documentation/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Pexels API{" "}
+          </a>{" "} which is not fully optimized to the German language but it works. 
+          I strive to keep on improving this page and helping others to learn
+          German. Viel Erfolg! 🍀
         </section>
         <footer className="App-footer">
           <small>
@@ -21,6 +48,7 @@ export default function App() {
             <a
               href="https://www.linkedin.com/in/beatriz-matafora-bab991108/"
               target="_blank"
+              title="Follow me on LinkedIn 😊"
               rel="noopener noreferrer"
             >
               Beatriz Matafora
@@ -29,6 +57,7 @@ export default function App() {
             <a
               href="https://github.com/servivo/dictionary-app"
               target="_blank"
+              title="Follow me on GitHub 😄"
               rel="noopener noreferrer"
             >
               GitHub
